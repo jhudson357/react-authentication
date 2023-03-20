069ad80237c8d519e4e542a85e9fc407c8edd39a
James Hudson <jmh357@comcast.net>
2023-03-20 12:36:05 -0500

    create action function that sends POST req to backend with signup credentials

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,3 +1,5 @@
+import { json, redirect } from 'react-router-dom'
+
 import AuthForm from '../components/AuthForm'
 
 function AuthenticationPage() {
@@ -5,3 +7,45 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage
+
+export async function action({request}) {
+  // action triggered anytime the AuthForm is submitted
+  // param 'request' is built-in with the <Form /> component from react-router-dom (there's also 'params')
+
+  // get the searchParam
+  const searchParams = new URL(request.url).searchParams
+  const mode = searchParams.get('mode') || 'login'
+
+  if (mode !== 'logn' && mode !== 'signup') {
+    throw json({message: 'Unsupported mode.'}, {status: 422})
+  }
+
+  // get the data submitted in the AuthForm
+  const data = await request.formData()
+  console.log(data)
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password')
+  }
+
+  // send the request to the backend
+  const response = await fetch('http://localhost:8080/' + mode, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(authData)
+  })
+
+  // handle the response
+  if (response.status === 422 || response.status === 401) {
+    return response
+  }
+
+  if (!response.ok) {
+    throw json({message: 'Could not authenticate user.'}, {status: 500})
+  }
+
+  // soon: manage that token
+  return redirect('/')
+}
